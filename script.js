7309cf1ffb227bee41d12cb753f3ef9e375abbec
beforwardaruba <beforwardaruba@gmail.com>
2025-07-04 03:17:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -65,15 +65,19 @@ document.addEventListener('DOMContentLoaded', function () {
             // Display the quote with a more structured format
             quoteContainer.innerHTML = `
                 <h3>📣 ${carModel}</h3>
-                <p>🇯🇵 <strong>Price - Auto + Shipping:</strong> ${formatCurrency(carValueAWG)} AWG</p>
-                <p>🇦🇷 <strong>Price - Invoerrecht:</strong> ${formatCurrency(importDutyAmount)} AWG</p>
+                <p>🚗+🚢 <strong>Price - Auto + Shipping:</strong> ${formatCurrency(carValueAWG)} AWG</p>
+                <p>📝 <strong>Price - Invoerrecht:</strong> ${formatCurrency(importDutyAmount)} AWG</p>
                 <p>🚗 <strong>Price - Handling fees:</strong> ${formatCurrency(handlingFee)} AWG</p>
-                <h4>💵 <strong>Final Price:</strong> ${formatCurrency(totalCost)} AWG</h4>
-                <p><strong>Step 1️⃣</strong> Make the first payment to Beforward Japan 🇯🇵 in US Dollar (USD ${formatCurrency(carValueUSD)} / ${formatCurrency(carValueAWG)} AWG) via wire transfer. If you need help, we are available anytime.</p>
-                <p><strong>Step 2️⃣</strong> Second payment to Beforward Aruba 🇦🇼 ( ${formatCurrency(importDutyAmount)} + Handling fee ${formatCurrency(handlingFee)}) = <strong>${formatCurrency(secondPaymentTotal)} AWG</strong> - Once the payment is received, the car and documentation will be sent via DHL to Aruba. The documents include the B/L (Bill of Lading) and Import Certificate.</p>
-                <p><strong>Step 3️⃣</strong> Enjoy your car in Aruba 🚗🔑</p>
+                <h4>💸 <strong>Final Price:</strong> ${formatCurrency(totalCost)} AWG</h4>
+                <p><strong>1️⃣</strong> E prome pago tin cu wordo manda pa Beforward Japon na US Dollar (USD ${formatCurrency(carValueUSD)} / ${formatCurrency(carValueAWG)} AWG) via un wire transfer. Si bo mester ayudo nos por assisti anytime.</p>
+                <p><strong>2️⃣</strong> Una bez e prome pago wordo confirma door di Beforward Japon nos lo manda bo C.A.P (Car Arrival Progress) Pa asina bo track e status di bo order i tambe wak e Shipping Schedule i tambe e Estimate Arrival Date.</p>
+                <p><strong>3️⃣</strong> E segundo pago tin cu wordo manda pa Beforward Aruba ( ${formatCurrency(importDutyAmount)} + Handling fee ${formatCurrency(handlingFee)}) = <strong>${formatCurrency(secondPaymentTotal)} AWG</strong> - Esaki ta wordo hasi prome cu e auto jega Aruba.</p>
+                <p><strong>4️⃣</strong> Documento pa clear out bo auto lo wordo manda for di Beforward Japon via DHL nos lo avisa bo ora esaki ta ready pa busca, esaki bo mester entrega na nos Office of nos por schedule pa pasa busce serka bo cas of trabow.</p>
+                <p><strong>5️⃣</strong> E proceso total ta dura 60 pa 90 dia si ta for di Japon. 90 pa 120 dia si ta for di Korea of Singapore.</p>
+                <p><strong>6️⃣</strong> Nos lo jamabo mane bo auto ta ready pa pickup na Aruba.</p>
                 <h5>💵 USD/AWG rate for today = ${usdToAwgRate}</h5>
-                <p><strong>🚨 Please ensure no delay in Step 2️⃣ to avoid additional costs. </strong></p>
+                <p><strong>🚨 Please zorg pa no tin delay cu stap 3️⃣ pa asina evita cu lo bin bewaking kosten pa cada dia cu e auto keda riba waf</strong></p>
+                <p><strong>⚠️ Disclaimer: Please note that shipping times may vary due to the nature of shipping via boat. While we strive to ensure timely delivery, delays may occur beyond our control due to weather conditions, port congestion, or other unforeseen circumstances during transit. We appreciate your understanding and patience.</strong></p>
             `;
 
             // Show the HTML and PDF download buttons
